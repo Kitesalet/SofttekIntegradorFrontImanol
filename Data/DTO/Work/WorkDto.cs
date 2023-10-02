28742e7ad72831ddb65ce70b9423ab0e5c3911aa
Imanol Echazarreta <kitesalett@gmail.com>
2023-10-02 05:33:23 -0300

    Retoques

--- Data/DTO/Work/WorkDto.cs
+++ Data/DTO/Work/WorkDto.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Data.DTO.Project;
-using Data.DTO.Service;
-
-namespace Data.DTO
+﻿namespace Data.DTO
 {
     public class WorkDto
     {
